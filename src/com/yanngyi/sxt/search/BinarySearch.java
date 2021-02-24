b20ce4dd79c680f03d961dba8f01591c01c5973f
伏兮若木 <605271205@qq.com>
2021-02-24 11:04:13 +0800

    1、二分查找课后练习实现

--- src/com/yanngyi/sxt/search/BinarySearch.java
+++ src/com/yanngyi/sxt/search/BinarySearch.java
@@ -1,5 +1,9 @@
 package com.yanngyi.sxt.search;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author yangyi
  */
@@ -12,6 +16,9 @@ public class BinarySearch {
         } else {
             System.out.println("找到了当前下标为："+ i);
         }
+        int[] arr2 = {1,2,3,4,5,6,7,7,7,7,7,7};
+        List<Integer> list = binarySearch2(arr2, 0, arr2.length - 1, 7);
+        System.out.println(list);
     }
 
     public static int binarySearch(int[] arr, int left, int right, int findValue){
@@ -29,5 +36,49 @@ public class BinarySearch {
         } else {
             return mid;
         }
+    }
+
+    /*
+     * 课后练习题 思考一个数组中存在多个同样的值
+     * 思路分析
+     * 1、在找到mid索引值，不要马上返回
+     * 2、向mid索引值的左边扫描，将所有等于findVal的元素的下标，加入到集合ArrayList
+     * 3、向mid索引值的右边扫描，将所有等于findVal的元素的下标，加入到集合ArrayList
+     * 4、将ArrayList返回*/
+
+     /**
+     * @param arr
+     * @param left
+     * @param right
+     * @param findValue
+     * @return
+     */
+    public static List binarySearch2(int[] arr, int left, int right, int findValue){
+        if (left > right) {
+            return new ArrayList<>();
+        }
+        int mid = (left + right)/2;
+        if (arr[mid] > findValue) {
+            return binarySearch2(arr, left, mid-1, findValue);
+        } else if (arr[mid] < findValue) {
+            return binarySearch2(arr, mid+1, right, findValue);
+        } else {
+            List<Integer> resIndexList = new ArrayList<>();
+            resIndexList.add(mid);
+            //左移查找
+            int temp = mid - 1;
+            while (temp >= 0 && arr[temp] == findValue) {
+                resIndexList.add(temp);
+                temp -= 1;
+            }
+            //右移查找
+            temp = mid + 1;
+            while (temp <= arr.length - 1 && arr[temp] == findValue) {
+                resIndexList.add(temp);
+                temp += 1;
+            }
+            return resIndexList;
+        }
+
     }
 }
